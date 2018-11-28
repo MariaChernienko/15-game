f0565e8ef392df31f84903487ab85bc52d27c44d
Maria <murikmarusia@gmail.com>
2018-11-28 16:50:02 +0200

    Added sort button

--- dist/js/main.js
+++ dist/js/main.js
@@ -23,31 +23,74 @@
     });
   };
 
-  gameCells[11].status = true;
+  gameCells[15].status = true;
   render(gameCells);
 
   window.addEventListener('keydown', (event) => {
     const arrowDirection = event.key;
+    console.log(gameCells.status);
 
     if (arrowDirection === 'ArrowLeft') {
-      for(let i = 0; i < gameCells.length; i++) {
+      for (let i = 0; i < gameCells.length; i++) {
         if (gameCells[i].status) {
-          let temp = gameCells[i-1];
-          gameCells[i-1] = gameCells[i];
+          const temp = gameCells[i - 1];
+          gameCells[i - 1] = gameCells[i];
           gameCells[i] = temp;
           render(gameCells);
         }
       }
     } else if (arrowDirection === 'ArrowRight') {
-      for(let i = 0; i < gameCells.length; i++) {
+      for (let i = 0; i < gameCells.length; i++) {
         if (gameCells[i].status) {
           console.log(gameCells[i]);
-          let temp = gameCells[i+1];
-          gameCells[i+1] = gameCells[i];
+          const temp = gameCells[i + 1];
+          gameCells[i + 1] = gameCells[i];
+          gameCells[i] = temp;
+          render(gameCells);
+          break;
+        }
+      }
+    } else if (arrowDirection === 'ArrowUp') {
+      for (let i = 0; i < gameCells.length; i++) {
+        if (gameCells[i].status) {
+          console.log(gameCells[i]);
+          const temp = gameCells[i - 4];
+          gameCells[i - 4] = gameCells[i];
           gameCells[i] = temp;
           render(gameCells);
         }
       }
+    } else if (arrowDirection === 'ArrowDown') {
+      for (let i = 0; i < gameCells.length; i++) {
+        if (gameCells[i].status) {
+          console.log(gameCells[i]);
+          const temp = gameCells[i + 4];
+          gameCells[i + 4] = gameCells[i];
+          gameCells[i] = temp;
+          render(gameCells);
+          break;
+        }
+      }
     }
+    console.log(gameCells);
+  });
+
+
+  const btn = document.querySelector('button');
+  function shuffle(arr) {
+    let j; let temp;
+    for (let i = arr.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      temp = arr[j];
+      arr[j] = arr[i];
+      arr[i] = temp;
+    }
+    return arr;
+  }
+
+  btn.addEventListener('click', () => {
+    shuffle(gameCells);
+    render(gameCells);
+    console.log(gameCells);
   });
 }());
